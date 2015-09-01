c4f4b0193e9ac4c46a9f79ff98bb4fbb885f54f9
Lars <lars.fjermeros@gmail.com>
2015-09-01 10:36:56 +0200

    Added all blurred images to the form

--- EyeTracking/Test/Form1.Designer.cs
+++ EyeTracking/Test/Form1.Designer.cs
@@ -40,7 +40,7 @@ namespace Test
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
 
-            this.ClientSize = new System.Drawing.Size(1121, 468);
+            this.ClientSize = new System.Drawing.Size(1200, 800);
             this.Name = "Form1";
             this.Text = "Form1";
             this.ResumeLayout(false);
--- EyeTracking/Test/Form1.cs
+++ EyeTracking/Test/Form1.cs
@@ -1,14 +1,11 @@
 ﻿using System.Drawing;
+using System.Linq;
 using System.Windows.Forms;
 
 namespace Test
 {
     public partial class Form1 : Form
     {
-        private const int EyeRadius = 75;
-        private const int EyeOuterRadius = 200;
-        private const int BackgroundRadius = 500;
-
         public Form1()
         {
             InitializeComponent();
@@ -19,11 +16,22 @@ namespace Test
             base.OnPaint(e); 
 
             var graphics = this.CreateGraphics();
-            var image = Image.FromFile("images/blur-0.jpg");
+            var point = new Point(Width / 2, Height / 2);
 
-            var eyeImage = new CircleImage(image, graphics);
-            var point = new Point(Width/2, Height/2);
-            eyeImage.DrawCircle(point, EyeRadius);
+            var images = new[] {"blur-0.jpg", "blur-20.jpg", "blur-50.jpg", "blur-80.jpg"};
+            var radiusArray = new[] {75, 200, 450, 800};
+
+            var radiusReverse = radiusArray.Reverse().ToArray();
+
+            var i = 0;
+            foreach (var imageName in images.Reverse())
+            {
+                var image = Image.FromFile("images/" + imageName);
+                var eyeImage = new CircleImage(image, graphics);
+                eyeImage.DrawCircle(point, radiusReverse[i]);
+                i++;
+            }
+
         }
     }
 }
