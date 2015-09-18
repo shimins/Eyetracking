4b93b044706881121481ab52695661eee197b3ff
shimins <shimins@stud.ntnu.no>
2015-09-18 14:10:44 +0200

    Added missing files

---
+++ EyeTracking/ImageBlur/Blur.cs
@@ -0,0 +1,158 @@
+﻿using System;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.Runtime.InteropServices;
+
+namespace ImageBlur
+{
+    public class Blur
+    {
+        private static double[,] filterMatrix;
+
+        private const int factor = 1;
+        private const int bias = 0;
+
+        public static Bitmap GaussianBlur(Bitmap sourceBitmap, int blurLevel)
+        {
+            filterMatrix = MatrixCalculator.Calculate(blurLevel, 1);
+
+            Console.WriteLine(sourceBitmap.Size);
+            Console.WriteLine(filterMatrix);
+
+            var sourceData = sourceBitmap.LockBits(new Rectangle(0, 0,
+                                                 sourceBitmap.Width, sourceBitmap.Height),
+                                                                   ImageLockMode.ReadOnly,
+                                                             PixelFormat.Format32bppArgb);
+
+            var pixelBuffer = new byte[sourceData.Stride * sourceData.Height];
+            var resultBuffer = new byte[sourceData.Stride * sourceData.Height];
+
+            Marshal.Copy(sourceData.Scan0, pixelBuffer, 0, pixelBuffer.Length);
+            sourceBitmap.UnlockBits(sourceData);
+
+            int filterWidth = filterMatrix.GetLength(1);
+            int filterHeight = filterMatrix.GetLength(0);
+
+            int filterOffset = (filterWidth - 1) / 2;
+
+            for (int offsetY = filterOffset; offsetY <
+                sourceBitmap.Height - filterOffset; offsetY++)
+            {
+                for (int offsetX = filterOffset; offsetX <
+                    sourceBitmap.Width - filterOffset; offsetX++)
+                {
+                    double blue = 0;
+                    double green = 0;
+                    double red = 0;
+
+                    var byteOffset = offsetY *
+                                     sourceData.Stride +
+                                     offsetX * 4;
+
+                    for (int filterY = -filterOffset;
+                        filterY <= filterOffset; filterY++)
+                    {
+                        for (int filterX = -filterOffset;
+                            filterX <= filterOffset; filterX++)
+                        {
+
+                            var calcOffset = byteOffset +
+                                             (filterX * 4) +
+                                             (filterY * sourceData.Stride);
+
+                            blue += pixelBuffer[calcOffset] *
+                                    filterMatrix[filterY + filterOffset,
+                                                        filterX + filterOffset];
+
+                            green += pixelBuffer[calcOffset + 1] *
+                                     filterMatrix[filterY + filterOffset,
+                                                        filterX + filterOffset];
+
+                            red += pixelBuffer[calcOffset + 2] *
+                                   filterMatrix[filterY + filterOffset,
+                                                      filterX + filterOffset];
+                        }
+                    }
+
+                    blue = factor * blue + bias;
+                    green = factor * green + bias;
+                    red = factor * red + bias;
+
+                    blue = (blue > 255 ? 255 : (blue < 0 ? 0 : blue));
+                    green = (green > 255 ? 255 : (green < 0 ? 0 : green));
+                    red = (red > 255 ? 255 : (red < 0 ? 0 : red));
+
+                    resultBuffer[byteOffset] = (byte)(blue);
+                    resultBuffer[byteOffset + 1] = (byte)(green);
+                    resultBuffer[byteOffset + 2] = (byte)(red);
+                    resultBuffer[byteOffset + 3] = 255;
+                }
+            }
+
+            var resultBitmap = new Bitmap(sourceBitmap.Width, sourceBitmap.Height);
+
+            var resultData = resultBitmap.LockBits(new Rectangle(0, 0,
+                                     resultBitmap.Width, resultBitmap.Height),
+                                                      ImageLockMode.WriteOnly,
+                                                 PixelFormat.Format32bppArgb);
+
+            Marshal.Copy(resultBuffer, 0, resultData.Scan0, resultBuffer.Length);
+            resultBitmap.UnlockBits(resultData);
+
+            return resultBitmap;
+        }
+
+
+        private static Bitmap BlurRect(Bitmap image, Rectangle rectangle, int blurSize)
+        {
+            Bitmap blurred = new Bitmap(image.Width, image.Height);
+
+            // make an exact copy of the bitmap provided
+            using (Graphics graphics = Graphics.FromImage(blurred))
+                graphics.DrawImage(image, new Rectangle(0, 0, image.Width, image.Height),
+                    new Rectangle(0, 0, image.Width, image.Height), GraphicsUnit.Pixel);
+
+            // look at every pixel in the blur rectangle
+            for (var xx = rectangle.X; xx < rectangle.X + rectangle.Width; xx += blurSize)
+            {
+                for (var yy = rectangle.Y; yy < rectangle.Y + rectangle.Height; yy += blurSize)
+                {
+                    int avgR = 0, avgG = 0, avgB = 0;
+                    var blurPixelCount = 0;
+
+                    // average the color of the red, green and blue for each pixel in the
+                    // blur size while making sure you don't go outside the image bounds
+                    for (var x = xx; (x < xx + blurSize && x < image.Width); x++)
+                    {
+                        for (var y = yy; (y < yy + blurSize && y < image.Height); y++)
+                        {
+                            var pixel = blurred.GetPixel(x, y);
+
+                            avgR += pixel.R;
+                            avgG += pixel.G;
+                            avgB += pixel.B;
+
+                            blurPixelCount++;
+                        }
+                    }
+
+                    avgR = avgR / blurPixelCount;
+                    avgG = avgG / blurPixelCount;
+                    avgB = avgB / blurPixelCount;
+
+                    // now that we know the average for the blur size, set each pixel to that color
+                    for (var x = xx; x < xx + blurSize && x < image.Width && x < rectangle.Width; x++)
+                        for (var y = yy; y < yy + blurSize && y < image.Height && y < rectangle.Height; y++)
+                            blurred.SetPixel(x, y, Color.FromArgb(avgR, avgG, avgB));
+                }
+            }
+
+            return blurred;
+        }
+
+        public static Bitmap BlurImage(Bitmap image, int blurSize)
+        {
+            return BlurRect(image, new Rectangle(0, 0, image.Width, image.Height), blurSize);
+        }
+    }
+}
