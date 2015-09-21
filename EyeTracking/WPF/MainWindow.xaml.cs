0f7658888c3380fe37c62d7161dab2f324fc4e1d
Lars <lars.fjermeros@gmail.com>
2015-09-21 13:03:49 +0200

    Add and remove

--- EyeTracking/WPF/MainWindow.xaml.cs
+++ EyeTracking/WPF/MainWindow.xaml.cs
@@ -1,17 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace WPF
 {
@@ -20,9 +11,39 @@ namespace WPF
     /// </summary>
     public partial class MainWindow : Window
     {
+        private readonly List<CircleImage> Images = new List<CircleImage>();
+        private const int ImageCount = 5;
+        private const int Radius = 400;
+
         public MainWindow()
         {
             InitializeComponent();
+
+            SetNewImageSet(ImageCount, "images/nature/");
+            InvalidateVisual();
+        }
+
+        private void SetNewImageSet(int imageCount, string imageFolder)
+        {
+            var factor = 500 / imageCount;
+            for (var i = ImageCount; i >= 0; i = i - 1)
+            {
+                var bitmapImage = new BitmapImage(new Uri(imageFolder + "Nature-" + i + ".jpg", UriKind.Relative));
+                var img = ImageHelper.ResizeImage(bitmapImage, new Size(Width, Height));
+                var r = (i * factor) + Radius;
+                Images.Add(new CircleImage(img, r));
+            }
+        }
+
+        protected override void OnRender(DrawingContext drawingContext)
+        {
+            base.OnRender(drawingContext);
+
+            var point = new Point(400, 600);
+            foreach (var image in Images)
+            {
+                image.DrawCircle(point, drawingContext);
+            }
         }
     }
 }
